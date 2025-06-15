49830a417461a66690bc7da0673459de75973147
tarabarnett <taralynnbarnett00@gmail.com>
2025-06-15 14:36:37 -0400

    fixed error in lusitana font style

--- app/ui/acme-logo.tsx
+++ app/ui/acme-logo.tsx
@@ -3,7 +3,8 @@ import { lusitana } from '@/app/ui/fonts';
 
 export default function AcmeLogo() {
   return (
-    <div
+    <div>
+    <p
       className={`${lusitana.className} flex flex-row items-center leading-none text-white`}
     >
       <GlobeAltIcon className="h-12 w-12 rotate-[15deg]" />
